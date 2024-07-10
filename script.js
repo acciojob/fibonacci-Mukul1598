6bef54f6742fd38a70f272ad4ac6512ea19d46e5
Mukul Singh <135323609+Mukul1598@users.noreply.github.com>
2024-07-10 09:57:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function fibonacci(num) {
-	// your code here
-  let arr = [0, 1];
-  for (let i = 2; i <= num; i++) {
-    arr.push(arr[i - 2] + arr[i - 1]);
-  }
-  return arr[num];
-}
-
 // function fibonacci(num) {
-// 	  your code here
-//   if (num === 0 || num === 1) return num;
-
-//   return fibonacci(num - 1) + fibonacci(num - 2);
+	// your code here
+//   let arr = [0, 1];
+//   for (let i = 2; i <= num; i++) {
+//     arr.push(arr[i - 2] + arr[i - 1]);
+//   }
+//   return arr[num];
 // }
+
+function fibonacci(num) {
+	  your code here
+  if (num === 0 || num === 1) return num;
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
